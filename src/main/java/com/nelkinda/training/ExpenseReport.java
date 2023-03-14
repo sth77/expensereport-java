75dbc4bfe070adbc5039c1f5c25604fbd7071bb1
Heinzer Stefan <stefan.heinzer@elca.ch>
2023-03-14 23:06:52 +0100

    Split calculation and presentation

--- src/main/java/com/nelkinda/training/ExpenseReport.java
+++ src/main/java/com/nelkinda/training/ExpenseReport.java
@@ -1,11 +1,28 @@
 package com.nelkinda.training;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 enum ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+    DINNER("Dinner", 5000, true),
+    BREAKFAST("Breakfast", 1000, true),
+    CAR_RENTAL("Car Rental", 0, false);
+
+    ExpenseType(String label, int limit, boolean meal) {
+        this.label = label;
+        this.limit = limit;
+        this.meal = meal;
+    }
+
+    String label;
+    int limit;
+    boolean meal;
+
+    boolean isMeal() {
+        return meal;
+    }
 }
 
 class Expense {
@@ -18,12 +35,16 @@ class Expense {
         validate();
     }
 
-    public ExpenseType getType() {
+    ExpenseType getType() {
         return type;
     }
 
-    public int getAmount() {
+    int getAmount() {
         return amount;
+    }
+
+    boolean exeedsLimit() {
+        return type.limit > 0 && amount > type.limit;
     }
 
     private void validate() {
@@ -36,46 +57,81 @@ class Expense {
     }
 }
 
-public class ExpenseReport {
+record ExpenseVM(String name, int amount, boolean mark) {
+}
+
+class ReportVM {
+    private List<ExpenseVM> expenses = new ArrayList<>();
+    private int total = 0;
+    private int mealExpenses = 0;
+
+    static ReportVM of(List<Expense> expenses) {
+        ReportVM result = new ReportVM();
+        expenses.forEach(result::add);
+        return result;
+    }
+
+    void add(Expense expense) {
+        expenses.add(new ExpenseVM(
+                expense.getType().label,
+                expense.getAmount(),
+                expense.exeedsLimit()));
+        total += expense.getAmount();
+        if (expense.getType().isMeal()) {
+            mealExpenses += expense.getAmount();
+        }
+    }
+
+    public int getMealExpenses() {
+        return mealExpenses;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public List<ExpenseVM> getExpenses() {
+        return expenses;
+    }
+
+}
+
+class ReportPrinter {
+
+    private final String MARK = "X";
+    private final String NO_MARK = " ";
 
     private final PrintStream output;
 
-    ExpenseReport(PrintStream output) {
+    ReportPrinter(PrintStream output) {
         this.output = output;
     }
 
-    public void printReport(List<Expense> expenses) {
-        int total = 0;
-        int mealExpenses = 0;
-
+    public void print(ReportVM report) {
         output.println("Expenses " + new Date());
+        report.getExpenses().forEach(this::print);
+        output.println("Meal expenses: " + report.getMealExpenses());
+        output.println("Total expenses: " + report.getTotal());
+    }
 
-        for (Expense expense : expenses) {
-            if (expense.getType() == ExpenseType.DINNER || expense.getType() == ExpenseType.BREAKFAST) {
-                mealExpenses += expense.getAmount();
-            }
+    private void print(ExpenseVM expense) {
+        output.println(String.format("%s\t%d\t%s",
+                expense.name(),
+                expense.amount(),
+                expense.mark() ? MARK : NO_MARK));
+    }
+}
 
-            String expenseName = "";
-            switch (expense.getType()) {
-            case DINNER:
-                expenseName = "Dinner";
-                break;
-            case BREAKFAST:
-                expenseName = "Breakfast";
-                break;
-            case CAR_RENTAL:
-                expenseName = "Car Rental";
-                break;
-            }
+public class ExpenseReport {
 
-            String mealOverExpensesMarker = expense.getType() == ExpenseType.DINNER && expense.getAmount() > 5000 || expense.getType() == ExpenseType.BREAKFAST && expense.getAmount() > 1000 ? "X" : " ";
+    private final ReportPrinter reportPrinter;
 
-            output.println(expenseName + "\t" + expense.getAmount() + "\t" + mealOverExpensesMarker);
+    ExpenseReport(PrintStream output) {
+        this.reportPrinter = new ReportPrinter(output);
+    }
 
-            total += expense.getAmount();
-        }
-
-        output.println("Meal expenses: " + mealExpenses);
-        output.println("Total expenses: " + total);
+    public void printReport(List<Expense> expenses) {
+        ReportVM report = ReportVM.of(expenses);
+        reportPrinter.print(report);
     }
 }
